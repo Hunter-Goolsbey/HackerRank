ac3c803af962abb61d3e1345a62e7dad8c931b83
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-11-12 16:44:11 -0800

    Update LeftRotation.py

--- LeftRotation.py
+++ LeftRotation.py
@@ -1,7 +1,12 @@
+#Rotate array to the left challenge
+
 def rotateLeft(d, arr):
+    
     z = []
-    for i in range(-1*(len(arr)-d), d):
+    
+    for i in range(-1 * (len(arr) - d), d):
         z.append(arr[i])
+        
     return(z)
   
   
